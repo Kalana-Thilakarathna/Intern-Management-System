071e9f1a7b2cf5e6966cfa04dd8dfc0f122db9ee
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2024-01-28 12:24:15 +0530

    publish ready

--- Client/src/Common/Header.jsx
+++ Client/src/Common/Header.jsx
@@ -1,45 +1,37 @@
-import React from 'react'
-import './Header.css'
-import 'font-awesome/css/font-awesome.min.css'
+import React from "react";
+import "./Header.css";
+import "font-awesome/css/font-awesome.min.css";
 
 function Header() {
   return (
-    <div className='common-header'>
-        <div className='component-1'>
-          <div className='header-border-right'>
-          <img src='../images/logo.png' alt='logo' className='header-logo'/>
-          <div className='header-user'>
-
-            <button className='header-log-out'>logout</button>
+    <div className="common-header">
+      <div className="component-1">
+        <div className="header-border-right">
+          <img src="../images/logo.png" alt="logo" className="header-logo" />
+          <div className="header-user">
+            <button className="header-log-out">logout</button>
             <i className="fa fa-user user-icon"></i>
-
           </div>
-          
-
-
-          </div>
-          
-          <img src='../images/uni.png' alt='header-uniname' className='header-uniname'/>
         </div>
 
-        <div className='component-2'>
-            <h1 className='header-title'>
-              University of Sri Jayawardenepura<br />
-                  Faculty of Technology<br />
-              Intern Management System
-            </h1>
+        <img
+          src="../images/uni.png"
+          alt="header-uniname"
+          className="header-uniname"
+        />
+      </div>
 
-            
-
-             
-
-
-
-
-        </div>
-      
+      <div className="component-2">
+        <h1 className="header-title">
+          University Name
+          <br />
+          Faculty Name
+          <br />
+          Intern Management System
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- Client/src/Loginpage.jsx
+++ Client/src/Loginpage.jsx
@@ -1,14 +1,13 @@
 import { useState, useContext, useEffect } from "react";
 import axios from "./Api/Axios";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-
+import logo from "../Images/logo.png";
 import useAuth from "./Hooks/useAuth";
-
 import Student from "./Student";
 import Company from "./Company";
 import Admin from "./Admin";
-
 import "./Loginpage.css";
+
 function Loginpage() {
   const { setAuth } = useAuth();
   const [userName, setUserName] = useState("");
@@ -49,63 +48,62 @@ function Loginpage() {
       <header className="header">
         <div className="w-screen header_line"></div>
         <div className="w-screen header_content flex flex-col items-center justify-center mt-2z">
-          <img
-            src="https://www.sjp.ac.lk/wp-content/uploads/2020/10/usjp-logo.png"
-            alt="University Logo"
-            className="uni-logo"
-          />
+          <img src={logo} alt="University Logo" className="uni-logo" />
           <div className="header_title">Intern Management System</div>
-          <div className="header_subtitle">Faculty of Technology</div>
-          <div className="header_subtitle">
-            University of Sri Jayewardenepura
-          </div>
+          <div className="header_subtitle">Faculty Name</div>
+          <div className="header_subtitle">University Name</div>
         </div>
       </header>
-      <main className="  bg-white border rounded-lg shadow-lg  content-container formH">
-        <form
-          className="flex flex-col items-center"
-          style={{ flex: 1 }}
-          onSubmit={HandleSubmit}
-        >
-          <h1>Log in</h1>
-          <div className="entryArea-1">
-            <div className="container-1">
-              <input
-                type="text"
-                name="userName"
-                id="userName"
-                className="userName"
-                required
-                value={userName}
-                onChange={(e) => {
-                  setUserName(e.target.value);
-                }}
-              />
-              <div className="input-Label">User Name</div>
+      <div className="mainbody">
+        <main className="  bg-white border rounded-lg shadow-lg  content-container formH">
+          <form
+            className="flex flex-col items-center formst"
+            style={{ flex: 1 }}
+            onSubmit={HandleSubmit}
+          >
+            <h1>Log in</h1>
+            <div className="entryArea-1">
+              <div className="container-1">
+                <input
+                  type="text"
+                  name="userName"
+                  id="userName"
+                  className="userName"
+                  required
+                  value={userName}
+                  onChange={(e) => {
+                    setUserName(e.target.value);
+                  }}
+                />
+                <div className="input-Label">User Name</div>
+              </div>
             </div>
-          </div>
-          <div className="entryArea-2">
-            <div className="container-2">
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className=" password"
-                required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <div className="input-Label">Password</div>
+            <div className="entryArea-2">
+              <div className="container-2">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className=" password"
+                  required
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <div className="input-Label">Password</div>
+              </div>
             </div>
-          </div>
 
-          {errorMessage && <div className="error-message">{errorMessage}</div>}
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
 
-          <button className="submit_button">Login</button>
-        </form>
-      </main>
+            <button className="submit_button">Login</button>
+          </form>
+        </main>
+      </div>
+
       <footer className="footer"></footer>
     </>
   );
